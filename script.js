8ea7ff08016a558d0dec88d52e56deac1a48f094
OliviaCFoskin <154926800+OliviaCFoskin@users.noreply.github.com>
2023-12-27 13:59:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var wordSets = [
-  ["Why dont we watch some anime"],
-  ["Why dont we chill by the deck door"],
-  ["Why dont we take a shower"],
-  ["Why dont we meditate"],
-  ["Why dont we go for a walk"],
-  ["Why dont we play with Olive"],
-  ["Why dont we do laundry"],
-  ["Why dont we take a nap"],
-  ["Why dont we go read a book"]
+  ["watch some anime"],
+  ["chill by the deck door"],
+  ["take a shower"],
+  ["meditate"],
+  ["go for a walk"],
+  ["play with Olive"],
+  ["do laundry"],
+  ["take a nap"],
+  ["go read a book"]
 ];
 
 var currentWordSetIndex = 0;
@@ -17,18 +17,16 @@ function flipWord() {
   var currentWordSet = wordSets[currentWordSetIndex];
 
   var words = sentenceElement.textContent.split(" ");
-  words.splice(0, currentWordSet.length, ...currentWordSet);
+
+  for (var i = 3; i < words.length; i++) {
+    words[i] = currentWordSet[i - 3];
+  }
 
   sentenceElement.innerHTML = words.join(" ");
 
   currentWordSetIndex = (currentWordSetIndex + 1) % wordSets.length;
 
-  // Check if all sets have been flipped, then reset
   if (currentWordSetIndex === 0) {
     resetWordSet();
   }
-}
-
-function resetWordSet() {
-  currentWordSetIndex = 0;
 }
