fc01fc3081aa8f04f768185389a2bba073c57022
OliviaCFoskin <154926800+OliviaCFoskin@users.noreply.github.com>
2023-12-27 13:33:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 var wordSets = [
-  ["read a book"],
-  ["watch some anime"],
-  ["chill by the deck door"],
-  ["take a shower"],
-  ["meditate"],
-  ["go for a walk"],
-  ["play with Olive"],
-  ["do laundry"],
-  ["take a nap"]
+  ["Why dont we watch some anime"],
+  ["Why dont we chill by the deck door"],
+  ["Why dont we take a shower"],
+  ["Why dont we meditate"],
+  ["Why dont we go for a walk"],
+  ["Why dont we play with Olive"],
+  ["Why dont we do laundry"],
+  ["Why dont we take a nap"],
+  ["Why dont we go read a book"]
 ];
 
 var currentWordSetIndex = 0;
 
 function flipWord() {
   var sentenceElement = document.getElementById("sentence");
-  function flipWord() {
-   var sentenceElement = document.getElementById("sentence");
-   var currentWordSet = wordSets[currentWordSetIndex];
+  var currentWordSet = wordSets[currentWordSetIndex];
 
-   var words = sentenceElement.textContent.split(" ");
-   words.splice(0, currentWordSet.length, ...currentWordSet);
+  var words = sentenceElement.textContent.split(" ");
+  words.splice(0, currentWordSet.length, ...currentWordSet);
 
-   sentenceElement.innerHTML = words.join(" ");
+  sentenceElement.innerHTML = words.join(" ");
 
-   currentWordSetIndex = (currentWordSetIndex + 1) % wordSets.length;
+  currentWordSetIndex = (currentWordSetIndex + 1) % wordSets.length;
+
+  // Check if all sets have been flipped, then reset
+  if (currentWordSetIndex === 0) {
+    resetWordSet();
+  }
+}
+
+function resetWordSet() {
+  currentWordSetIndex = 0;
 }
