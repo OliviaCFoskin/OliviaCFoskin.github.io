b64b2a999a8b3541ba35acd244e1be20d5ce5526
OliviaCFoskin <154926800+OliviaCFoskin@users.noreply.github.com>
2023-12-27 11:59:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,21 +14,14 @@ var currentWordSetIndex = 0;
 
 function flipWord() {
   var sentenceElement = document.getElementById("sentence");
-  var flippedElement = document.createElement("span");
-  flippedElement.classList.add("flipped");
+  function flipWord() {
+   var sentenceElement = document.getElementById("sentence");
+   var currentWordSet = wordSets[currentWordSetIndex];
 
-  var currentWordSet = wordSets[currentWordSetIndex];
+   var words = sentenceElement.textContent.split(" ");
+   words.splice(0, currentWordSet.length, ...currentWordSet);
 
- var words = sentenceElement.textContent.split(" ");
-  words.splice(0, currentWordSet.length, ...currentWordSet);
+   sentenceElement.innerHTML = words.join(" ");
 
-  flippedElement.innerHTML = words.join(" ");
-
-  if (sentenceElement.childNodes.length === 1) {
-    sentenceElement.appendChild(flippedElement);
-  } else {
-    sentenceElement.removeChild(sentenceElement.childNodes[1]);
-  }
-
-  currentWordSetIndex = (currentWordSetIndex + 1) % wordSets.length;
+   currentWordSetIndex = (currentWordSetIndex + 1) % wordSets.length;
 }
